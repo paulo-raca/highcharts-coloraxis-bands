65b2edd5e63c5e35830ec70866e5745bd02c089d
Paulo Costa <me@paulo.costa.nom.br>
2017-05-14 01:25:11 -0300

    Refactoring

--- highcharts-coloraxis-bands.js
+++ highcharts-coloraxis-bands.js
@@ -60,53 +60,54 @@ wrap(ColorAxis.prototype, 'initStops', function (proceed) {
 function remapStops (stops, ticks, banding) {
 	var ret = [];
 
-	var min = 0;
 	for (var i=0; i<=ticks.length; i++) {
 		var min = i === 0 ? 0 : ticks[i-1],
 			max = i === ticks.length ? 1 : ticks[i],
 			bandMin = i == 0 ? min : banding / 2 * (max - min) + min,
-			bandMax = i == ticks.length ? max : banding / 2 * (min - max) + max,
-			subStops = stops.slice();
+			bandMax = i == ticks.length ? max : banding / 2 * (min - max) + max;
 
 		if (min == max) continue;
 
-		while (subStops.length >= 2 && subStops[1][0] <= min) {
-			subStops.shift();
-		}
-		while (subStops.length >= 2 && subStops[subStops.length - 2][0] >= max) {
-			subStops.pop();
-		}
-
-		if (subStops.length > 1) {
-			if (subStops[0][0] < min) {
-				subStops[0] = [
-					min,
-					ColorAxis.prototype.tweenColors(
-						subStops[0].color,
-						subStops[1].color,
-						(min - subStops[0][0]) / (subStops[1][0] - subStops[0][0])
-					)
-				];
-				subStops[0].color = color(subStops[0][1]);
-			}
-			if (subStops[subStops.length-1][0] > max) {
-				subStops[subStops.length-1] = [
-					max,
-					ColorAxis.prototype.tweenColors(
-						subStops[subStops.length-2].color,
-						subStops[subStops.length-1].color,
-						(max - subStops[subStops.length-2][0]) / (subStops[subStops.length-1][0] - subStops[subStops.length-2][0])
-					)
-				];
-				subStops[subStops.length-1].color = color(subStops[subStops.length-1][1]);
-			}
-		}
-		ret = ret.concat(remapStopRange(subStops, min, max, bandMin, bandMax));
+		ret = ret.concat(remapStopRange(stops, min, max, bandMin, bandMax));
 	}
 	return ret;
 };
 
 function remapStopRange (stops, min, max, newMin, newMax) {
+	stops = stops.slice();
+
+	while (stops.length >= 2 && stops[1][0] <= min) {
+		stops.shift();
+	}
+	while (stops.length >= 2 && stops[stops.length - 2][0] >= max) {
+		stops.pop();
+	}
+
+	if (stops.length > 1) {
+		if (stops[0][0] < min) {
+			stops[0] = [
+				min,
+				ColorAxis.prototype.tweenColors(
+					stops[0].color,
+					stops[1].color,
+					(min - stops[0][0]) / (stops[1][0] - stops[0][0])
+				)
+			];
+			stops[0].color = color(stops[0][1]);
+		}
+		if (stops[stops.length-1][0] > max) {
+			stops[stops.length-1] = [
+				max,
+				ColorAxis.prototype.tweenColors(
+					stops[stops.length-2].color,
+					stops[stops.length-1].color,
+					(max - stops[stops.length-2][0]) / (stops[stops.length-1][0] - stops[stops.length-2][0])
+				)
+			];
+			stops[stops.length-1].color = color(stops[stops.length-1][1]);
+		}
+	}
+
 	return stops.map(function (stop) {
 		var rel = (stop[0] - min) / (max - min),
 			newPos = rel * (newMax - newMin) + newMin;
